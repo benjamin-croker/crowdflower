1565d8c6df12438c780b6fde1a24fbfb151e49c3
Benjamin Croker <benjamin.croker@gmail.com>
2013-11-30 11:44:40 +1000

    Removed unused classifiers. Removing stopwords had a negative effect

--- clx.py
+++ clx.py
@@ -3,8 +3,7 @@ import numpy as np
 import os
 import pickle
 
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge
 from sklearn.cross_validation import KFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -14,36 +13,35 @@ from nltk.tokenize import RegexpTokenizer
 SEED = 42
 
 
-def remove_http_links(concat_tweets):
-    print("Removing HTTP links")
-    return [" ".join([w for w in tweets.lower().split(" ") if w[0:4] != "http"])
-            for tweets in concat_tweets]
+def lower_case(tweets):
+    print("Making tweets lower case")
+    return [tweet.lower() for tweet in tweets]
 
 
-def remove_stopwords(concat_tweets):
+def remove_stopwords(tweets):
     print("Removing Stopwords")
     tokenizer = RegexpTokenizer(r'\w+')
-    return [" ".join([w for w in tokenizer.tokenize(tweets.lower())
+    return [" ".join([w for w in tokenizer.tokenize(tweet.lower())
                       if w not in stopwords.words("english")])
-            for tweets in concat_tweets]
+            for tweet in tweets]
 
 
-def gen_cv_predictions(df, lin_preds_fn="lin_preds.pkl", ridge_preds_fn="ridge_preds.pkl", non_sparse=True):
+def gen_cv_predictions(df, ridge_preds_fn="ridge_preds.pkl"):
     """ Generates predictions for 10-fold cross validation
     """
 
     # generate the 10 fold splits
     kf = KFold(n=df.shape[0], n_folds=10, random_state=SEED, shuffle=True)
 
-    all_lin_preds = []
     all_ridge_preds = []
 
     fold_n = 0
 
-    # logistic regression model with defaults
-    lin_cl = Lasso()
-    # rf model
+    # ridge regression model
     ridge_cl = Ridge(alpha=3.0)
+
+    # pre-process the tweets
+    df["tweet"] = lower_case(df["tweet"])
 
     for train_indices, fold_eval_indices in kf:
         print("Evaluating fold {} of {}".format(fold_n+1, 10))
@@ -63,21 +61,11 @@ def gen_cv_predictions(df, lin_preds_fn="lin_preds.pkl", ridge_preds_fn="ridge_p
         # convert to float arrays
         y_train = np.array(y_train, dtype="float")
 
-        print("Training linear model")
-        lin_cl.fit(X_train, y_train)
-        # manually fit the Lasso regression
-        lin_preds = X_eval * np.transpose(lin_cl.sparse_coef_) + np.array(lin_cl.intercept_)
-
-        # save the predictions
-        all_lin_preds.append(lin_preds)
-        with open(lin_preds_fn, "wb") as f:
-            pickle.dump(all_lin_preds, f)
-
         print("Training ridge regression model")
         ridge_cl.fit(X_train, y_train)
         ridge_preds = ridge_cl.predict(X_eval)
 
-       # save the predictions
+        # save the predictions
         all_ridge_preds.append(ridge_preds)
         with open(ridge_preds_fn, "wb") as f:
             pickle.dump(all_ridge_preds, f)
@@ -85,18 +73,13 @@ def gen_cv_predictions(df, lin_preds_fn="lin_preds.pkl", ridge_preds_fn="ridge_p
         fold_n += 1
 
 
-def eval_model(df, lin_preds_fn="lin_preds.pkl", ridge_preds_fn="ridge_preds.pkl", weights=(0.1, 0.9)):
+def eval_model(df, lasso_preds_fn="lasso_preds.pkl", ridge_preds_fn="ridge_preds.pkl", weights=(0.05, 0.95)):
     """ evaluates the results of the 10 fold CV
     """
 
     # perform k-fold validation
     kf = KFold(n=df.shape[0], n_folds=10, random_state=SEED, shuffle=True)
-    rms_scores_lin = np.zeros(10)
     rms_scores_ridge = np.zeros(10)
-    rms_scores_comb = np.zeros(10)
-
-    with open(lin_preds_fn) as f:
-        all_lin_preds = pickle.load(f)
 
     with open(ridge_preds_fn) as f:
         all_ridge_preds = pickle.load(f)
@@ -109,15 +92,6 @@ def eval_model(df, lin_preds_fn="lin_preds.pkl", ridge_preds_fn="ridge_preds.pkl
         # convert to float arrays
         y_eval = np.array(y_eval, dtype="float")
 
-        lin_preds = np.array(all_lin_preds[fold_n])
-        # probabilities for certain predictions should sum to 1
-        # normalise the 'S' predictions
-        lin_preds[:, 0:5] /= lin_preds[:, 0:5].sum(1, keepdims=True)
-        # normalise the 'W' predictions
-        lin_preds[:, 5:9] /= lin_preds[:, 5:9].sum(1, keepdims=True)
-
-        rms_scores_lin[fold_n] = np.sqrt(np.sum(np.array(np.array(lin_preds-y_eval)**2)/(len(fold_eval_indices) * 24.0)))
-
         ridge_preds = all_ridge_preds[fold_n]
         # normalise the 'S' predictions
         ridge_preds[:, 0:5] /= ridge_preds[:, 0:5].sum(1, keepdims=True)
@@ -125,18 +99,47 @@ def eval_model(df, lin_preds_fn="lin_preds.pkl", ridge_preds_fn="ridge_preds.pkl
         ridge_preds[:, 5:9] /= ridge_preds[:, 5:9].sum(1, keepdims=True)
         rms_scores_ridge[fold_n] = np.sqrt(np.sum(np.array(np.array(ridge_preds-y_eval)**2)/(len(fold_eval_indices)*24.0)))
 
-        #combine predictions
-        comb_preds = weights[0]*lin_preds + weights[1]*ridge_preds
-        rms_scores_comb[fold_n] = np.sqrt(np.sum(np.array(np.array(comb_preds-y_eval)**2)/(len(fold_eval_indices)*24.0)))
-
         fold_n += 1
 
-    print("Mean Linear RMS error:{}, Std:{}".format(np.mean(rms_scores_lin), np.std(rms_scores_lin)))
     print("Mean Ridge RMS error:{}, Std:{}".format(np.mean(rms_scores_ridge), np.std(rms_scores_ridge)))
-    print("Mean Combined RMS error:{}, Std:{}".format(np.mean(rms_scores_comb), np.std(rms_scores_comb)))
+
+
+def submission(train_DF, test_DF, output_filename="submission.csv"):
+    ridge_cl = Ridge(alpha=3.0)
+
+    tfv = TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode',
+                          analyzer='word', token_pattern=r'\w{1,}',
+                          decode_error='ignore',
+                          ngram_range=(1, 1), use_idf=1, smooth_idf=1,
+                          sublinear_tf=1)
+
+    X_train = tfv.fit_transform(train_DF["tweet"])
+    X_test = tfv.transform(test_DF["tweet"])
+
+    # extract all the y values, which are in column 4 onwards
+    y_train = np.array(train_DF)[:, 4:]
+
+    # convert to float arrays
+    y_train = np.array(y_train, dtype="float")
+
+    print("Training ridge regression model")
+    ridge_cl.fit(X_train, y_train)
+    preds = ridge_cl.predict(X_test)
+
+    # make a submission
+    submission_DF = pd.DataFrame(data=preds, columns=train_DF.columns[4:])
+    # add the id column
+    submission_DF.insert(0, "id", test_DF["id"])
+    submission_DF.to_csv(output_filename, index=False)
+
+
 
 if __name__ == "__main__":
     #df = load_raw_tweets()
-    df = pd.read_csv(os.path.join("data", "train.csv"))
-    #gen_cv_predictions(df)
-    eval_model(df)
+    train_DF = pd.read_csv(os.path.join("data", "train.csv"))
+    test_DF = pd.read_csv(os.path.join("data", "test.csv"))
+
+    gen_cv_predictions(train_DF)
+    eval_model(train_DF)
+
+    #submission(train_DF, test_DF)
